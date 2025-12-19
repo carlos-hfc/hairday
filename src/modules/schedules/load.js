d0c7769985db6b578fbc6737df5c64c686fbcd69
Carlos Faustino <chfcchfc96@gmail.com>
2025-12-19 11:14:58 -0300

    feat: list schedules

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,15 @@
+import { listSchedulesByDay } from "../../services/list-schedules-by-day"
 import { hoursLoad } from "../form/hours-load"
+import { showSchedules } from "./show"
 
 const selectedDate = document.getElementById("date")
 
-export function schedulesDays() {
+export async function schedulesDays() {
   const date = selectedDate.value
+
+  const dailySchedules = await listSchedulesByDay({ date })
+
+  showSchedules({ dailySchedules })
 
   hoursLoad({ date })
 }
---
+++ src/services/list-schedules-by-day.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs"
+import { apiConfig } from "./api-config"
+
+export async function listSchedulesByDay({ date }) {
+  try {
+    const response = await fetch(`${apiConfig.baseURL}/schedules`)
+
+    const data = await response.json()
+
+    const dailySchedule = data.filter(schedule => dayjs(date).isSame(schedule.when, "day"))
+
+    return dailySchedule
+  } catch (error) {
+    console.log(error)
+    alert("Não foi possível buscar os agendamentos do dia selecionado.")
+  }
+}
